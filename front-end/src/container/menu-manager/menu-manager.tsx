0501689ae18eb22627f89c60f2de035f59c8fca8
Crom Borw <66833938+CromBorw2e36@users.noreply.github.com>
2023-05-21 18:10:44 +0700

    xây dựng bán hàng

--- front-end/src/container/menu-manager/menu-manager.tsx
+++ front-end/src/container/menu-manager/menu-manager.tsx
@@ -6,13 +6,22 @@ import BHButton from '../../component/BH/Button/BHButton';
 import { BhClient, ProductionModel, SysRoleRightModel } from '../../common/interface/BHInterface';
 import BHGroupButton from '../../component/BH/GroupButton/BHGroupButton';
 import BHDataGrid from '../../component/BH/DataGrid/BHDataGrid';
+import ActionMenuManager from '../actions/menu-manager/menu-manager';
+
+interface IActions {
+    open?: boolean | undefined;
+    idItemSelected?: string | undefined;
+    payload?: string | undefined;
+    data?: any;
+}
 
 const MenuManager = () => {
 
     const menuid = JSON.parse(localStorage.getItem("menuid") || "{}");
+
     const SessionLogin = JSON.parse(localStorage.getItem("UserLogin") || "{}");
 
-
+    const [action, setAction] = React.useState<IActions>({} as IActions);
 
     const [sysRoleRights, setSysRoleRights] = useState<SysRoleRightModel[]>([] as SysRoleRightModel[]);
 
@@ -22,10 +31,13 @@ const MenuManager = () => {
 
     const [dataTable, setDataTable] = useState<ProductionModel[]>([] as ProductionModel[]);
 
+    const [productSelect, setProductSelect] = useState<ProductionModel[]>([] as ProductionModel[]);
+
     React.useEffect(() => {
         if (menuid) {
             postApiAll("/api/SysMenuRoles/SysRoleRightGet", {
-                menuid: menuid.id
+                menuid: menuid.id,
+                show: "view"
             }
             ).then(res => {
                 setSysRoleRights(res);
@@ -42,25 +54,61 @@ const MenuManager = () => {
             ma_ch: SessionLogin.ma_ch,
         })
             .then(res => {
-               
+
                 setDataTable(res);
 
             })
             .catch(err => {
 
                 console.log(err)
-                
+
             })
 
     }, [reload])
 
-    const clickAction = (e: any) => {
-        console.log(e)
+    const clickAction = (data: any) => {
+        const { row, action } = data;
+        if (action === "SELECTED") {
+            setProductSelect([...row])
+        } else if (action !== "ADD") {
+            setAction({
+                ...action,
+                open: true,
+                data: row.data,
+                payload: action
+            });
+        }
+        else if (action === "ADD") {
+            setAction({
+                ...action,
+                open: true,
+                payload: action
+            });
+        } else if (action === "ADD") {
+            setAction({
+                ...action,
+                open: true,
+                payload: action
+            });
+        }
+
     }
 
-    
+
     return (
         <Box className="w-100 h-100">
+
+            {
+                React.useMemo(() => {
+                    return <ActionMenuManager
+                        action={action.payload}
+                        onClose={() => setAction({ ...action, open: false })}
+                        row={action.data}
+                        open={action.open}
+                    />
+                }, [action.open])
+            }
+
             <Navbar
                 title='Quản lý menu'
             />
@@ -68,10 +116,11 @@ const MenuManager = () => {
                 <BHGroupButton
                     roleRight={sysRoleRights}
                     clickAction={(e) => clickAction(e)}
+                    filterOptions={[]}
                 />
             }
             <BHDataGrid
-                key='id'
+                key='Id'
                 dataSource={dataTable}
                 HandleClick={(data) => clickAction(data)}
             />
